772c8e8f49fadadc07b19330b4a4982978d11ff9
MrTesla-Python <63614313+MrTesla-Python@users.noreply.github.com>
2023-02-25 16:39:43 -0500

    Completed bisection_search

--- Problem Set 2/problem_3.py
+++ Problem Set 2/problem_3.py
@@ -1,21 +1,23 @@
 balance = 320000
 annualInterestRate = 0.2
+monthlyInterestRate = 0.2/12
 backup_balance = balance
-monthlyInterestRate = annualInterestRate/12
 lower = backup_balance/12
-upper = (backup_balance * (1 + monthlyInterestRate)**12)/12.0
+upper = (balance * (1 + monthlyInterestRate)**12)/12
 epsilon = 0.03
-
+monthlyPayment = 0
 while abs(balance) > epsilon:
     balance = backup_balance
     monthlyPayment = (upper+lower)/2
     for i in range(12):
-        balance -= (monthlyPayment)
-        balance += ((monthlyInterestRate)*balance)
+        balance -= monthlyPayment
+        balance += monthlyInterestRate*balance
     if balance > epsilon:
         lower = monthlyPayment
-    elif balance < -epsilon:
+    elif balance < epsilon:
         upper = monthlyPayment
     else:
         break
-print(round(monthlyPayment, 2))
+    
+
+print(round(monthlyPayment,2))
