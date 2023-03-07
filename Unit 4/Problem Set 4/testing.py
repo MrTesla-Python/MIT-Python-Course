0dda43be5e4475ead36acb591df1d2cb35aa1962
MrTesla-Python <63614313+MrTesla-Python@users.noreply.github.com>
2023-03-06 20:51:13 -0500

    Scrabble

---
+++ Unit 4/Problem Set 4/testing.py
@@ -0,0 +1,52 @@
+SCRABBLE_LETTER_VALUES = {
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+}
+def getWordScore(word, n):
+    """
+    Returns the score for a word. Assumes the word is a valid word.
+
+    The score for a word is the sum of the points for letters in the
+    word, multiplied by the length of the word, PLUS 50 points if all n
+    letters are used on the first turn.
+
+    Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
+    worth 3, D is worth 2, E is worth 1, and so on (see SCRABBLE_LETTER_VALUES)
+
+    word: string (lowercase letters)
+    n: integer (HAND_SIZE; i.e., hand size required for additional points)
+    returns: int >= 0
+    """
+    word_value = 0
+    for i in word:
+        word_value += SCRABBLE_LETTER_VALUES[i]
+    word_value = word_value * len(word)
+    if len(word) == n:
+        word_value+=50
+    return word_value
+print(getWordScore("weed", 7))
+
+def updateHand(hand, word):
+    """
+    Assumes that 'hand' has all the letters in word.
+    In other words, this assumes that however many times
+    a letter appears in 'word', 'hand' has at least as
+    many of that letter in it. 
+
+    Updates the hand: uses up the letters in the given word
+    and returns the new hand, without those letters in it.
+
+    Has no side effects: does not modify hand.
+
+    word: string
+    hand: dictionary (string -> int)    
+    returns: dictionary (string -> int)
+    """
+    ans = hand.copy()
+    for i in word:
+        if i in ans.keys():
+            ans[i]-=1
+    return ans
+    
+hand = {'a':1, 'q':1, 'l':2, 'm':1, 'u':1, 'i':1}
+
+print(updateHand(hand, "quail"))
