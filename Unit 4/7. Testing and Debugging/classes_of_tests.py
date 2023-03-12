55b61aa41f2a588e1dfee6420cb5686deb5b223d
MrTesla-Python <63614313+MrTesla-Python@users.noreply.github.com>
2023-03-11 22:04:24 -0500

    Did some more debugging vids

---
+++ Unit 4/7. Testing and Debugging/classes_of_tests.py
@@ -0,0 +1,15 @@
+""" 
+Set yourself up for easy testing and debugging
+from the start, design code to ease this part
+break program into modules
+document very important
+
+When are you ready to test
+ensure code runs
+
+Black Box testing
+designed wothout looking at code
+can be done by someone else
+testing can be reused
+
+"""
